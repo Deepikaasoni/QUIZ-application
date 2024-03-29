b04e65599e4a0e58273673f9b45b33c94a727ffe
Deepikaasoni <109984417+Deepikaasoni@users.noreply.github.com>
2024-03-29 16:02:02 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { Button } from 'antd';
+import "./stylesheets/theme.css";
+import "./stylesheets/alignments.css";
+import "./stylesheets/textelements.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/form-elements.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./pages/common/Login";
+import Register from "./pages/common/Register";
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/common/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<ProtectedRoute>
+          <Home />
+        </ProtectedRoute>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
